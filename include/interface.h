04452bd8c6348e3b6316609ed00f6aa48a82dcae
alidor95 <128261284+alidor95@users.noreply.github.com>
2026-04-27 09:07:27 -0300

    Add files via upload

--- include/interface.h
+++ include/interface.h
@@ -1,1 +1,12 @@
-void mascarar_senha(void);
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#define BLOQUEADO 0
+#define LIBERADO  1
+#define ERRO      2
+
+extern unsigned char estado_atual;
+
+unsigned char mascarar_senha(unsigned char *senha_out, unsigned char tamanho_senha);
+
+#endif
